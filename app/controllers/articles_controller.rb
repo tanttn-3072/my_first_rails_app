6e2ffe2aeb1d59e84efb975cf8fe0722e8bbcce3
Tan Tran Truong Nhat <tran.truong.nhat.tan@sun-asterisk.com>
2024-03-21 23:47:19 +0900

    CRUD of a form before DRY

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -27,42 +27,26 @@ class ArticlesController < ApplicationController
       flash[:notice] = "Article was created successfully."
       redirect_to @article
     else
-      render 'new'  
+      render 'new', status: :unprocessable_entity
+    end  
     #redirect_to action: "index"
-    # @article = Article.new(article_params)
-
-    # respond_to do |format|
-    #   if @article.save
-    #     format.html { redirect_to article_url(@article), notice: "Article was successfully created." }
-    #     format.json { render :show, status: :created, location: @article }
-    #   else
-    #     format.html { render :new, status: :unprocessable_entity }
-    #     format.json { render json: @article.errors, status: :unprocessable_entity }
-    #   end
-    # end
+   
   end
 
   # PATCH/PUT /articles/1 or /articles/1.json
   def update
-    respond_to do |format|
-      if @article.update(article_params)
-        format.html { redirect_to article_url(@article), notice: "Article was successfully updated." }
-        format.json { render :show, status: :ok, location: @article }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @article.errors, status: :unprocessable_entity }
-      end
-    end
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = "Article was updated successfully."
+      redirect_to @article
+    else
+      render 'edit', status: :unprocessable_entity
+    end    
   end
 
   # DELETE /articles/1 or /articles/1.json
   def destroy
     @article.destroy!
-
-    respond_to do |format|
-      format.html { redirect_to articles_url, notice: "Article was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    redirect_to articles_path #redirect to inđex path, check the routes and see the link (first row)
   end
 
   private
