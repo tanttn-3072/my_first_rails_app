973096cba0f13f57f4dc6585a21d8a2d66a3501c
Tan Tran Truong Nhat <tran.truong.nhat.tan@sun-asterisk.com>
2024-03-27 23:27:01 +0900

    add sign up

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_15_133402) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_27_085657) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer "user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
