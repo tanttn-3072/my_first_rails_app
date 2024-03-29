035fe2fa99bf04104fb41ee8f102fee2f41c0b8d
Tan Tran Truong Nhat <tran.truong.nhat.tan@sun-asterisk.com>
2024-03-29 15:42:43 +0900

    destroy user and its dependence

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: %i[ show edit update ]
+  before_action :set_user, only: %i[ show edit update destroy ]
   before_action :require_user, only: %i[ edit update ]
-  before_action :require_same_user, only: %i[ edit update ]
+  before_action :require_same_user, only: %i[ edit update destroy ]
 
   def show
     @articles = @user.articles.paginate(page: params[:page], per_page: 5)
@@ -35,6 +35,13 @@ class UsersController < ApplicationController
       render 'new', status: :unprocessable_entity
     end      
   end  
+  
+  def destroy
+    @user.destroy!
+    session[:user_id] = nil
+    flash[:notice] = "Account and all associated articles successfully deleted"
+    redirect_to root_path
+  end
 
   private
   def user_params
